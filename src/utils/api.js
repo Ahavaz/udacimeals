146587b7a01d117d6a56725000f912b7827c1da1
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-18 14:32:36 -0300

    refactored api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,6 @@ export function fetchRecipes(food = '') {
   food = food.trim()
 
   return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
-    .then((res) => res.json())
+    .then(res => res.json())
     .then(({ hits }) => hits.map(({ recipe }) => recipe))
 }
