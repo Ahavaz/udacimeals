7fb419a5b7ada5224b603c342de9f01933f02df7
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-26 16:44:44 -0300

    moved trim function to helpers folder

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,18 +1,10 @@
 import React from 'react'
+import { trim } from '../utils/helpers'
 
-const trim = str => {
-  return str.length > 16
-    ? str.slice(0, 16) + '...'
-    : str
-}
-
-const FoodList = ({ food, onSelect }) => {
-  if (food.length === 0) {
-    return <p>Your search has 0 results.</p>
-  }
-
-  return (
-    <ul className='food-list'>
+const FoodList = ({ food, onSelect }) =>
+  food.length === 0
+    ? <p>Your search has 0 results.</p>
+    : <ul className='food-list'>
       {food.map((item) => (
         <li onClick={() => onSelect(item)} key={item.label}>
           <h3>{trim(item.label)}</h3>
@@ -22,7 +14,5 @@ const FoodList = ({ food, onSelect }) => {
         </li>
       ))}
     </ul>
-  )
-}
 
 export default FoodList
