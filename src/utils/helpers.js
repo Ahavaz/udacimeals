63d6ef87eab14b2aed8ab0da9cb54698d5582da3
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-26 16:44:09 -0300

    added trim

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,9 @@
-export function capitalize(str = '') {
-  return typeof str !== 'string'
+export const capitalize = (str = '') =>
+  typeof str !== 'string'
     ? ''
     : str[0].toUpperCase() + str.slice(1)
-}
+
+export const trim = (str, maxLength = 16) =>
+  str.length > maxLength
+    ? `${str.slice(0, maxLength)}...`
+    : str
